1c99aee49ae6d5bfa9c13db207e0bdaad92bffa9
Wajiha Begum <97507672+Wajiha99@users.noreply.github.com>
2025-09-30 01:26:08 +0530

    Update BS.java

--- BS.java
+++ BS.java
@@ -19,3 +19,26 @@ class Solution {
     return -1;
     }
 }
+2. Search Insert Position : 
+class Solution {
+    public int searchInsert(int[] A, int B) {
+       int n = A.length;
+        int l = 0;
+        int h = n - 1;
+        int ans = n;
+        while(l <= h){
+            int m = (l+h)/2;
+            if(A[m] == B){
+                return m;
+            }
+            else if(A[m] > B){
+                ans = m;
+                h = m - 1;
+            }
+            else{
+                l = m + 1;
+            }
+        }
+        return ans; 
+    }
+}
