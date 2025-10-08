b844a6c59b19b4a0d3631cb62970a9ee6bd4e6d6
Wajiha Begum <97507672+Wajiha99@users.noreply.github.com>
2025-10-08 19:42:29 +0530

    Update Binary_Search.java

--- Binary_Search.java
+++ Binary_Search.java
@@ -235,5 +235,38 @@ class Solution {
      return last;
  }
 }
+9.Find Peak Element
+    class Solution {
+    public int findPeakElement(int[] nums) {
+     int n = nums.length;
+    if(n == 1){
+        return 0;
+     }
+     if(nums[0] > nums[1]){
+        return 0;
+     }
+     if(nums[n-1] > nums[n-2]){
+        return n-1;
+     }
+     int l = 1;
+     int h = n-2;
+     while(l <= h){
+        int m = (l+h)/2;
+        if(nums[m] > nums[m-1] && nums[m] > nums[m+1]){
+            return m;
+        }
+        else if(nums[m] < nums[m+1]){
+            //go to right
+            l = m + 1;
+        }
+        else{
+            //go to left 
+            h = m - 1;
+        }
+     }   
+     return -1;
+    }
+}
+
 
   
