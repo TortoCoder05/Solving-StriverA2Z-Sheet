295397dadc70a47d8bfe57cb41097c63586a4261
Wajiha Begum <97507672+TortoCoder05@users.noreply.github.com>
2025-10-23 15:51:31 +0530

    Update Binary_Search.java

--- Binary_Search.java
+++ Binary_Search.java
@@ -433,4 +433,24 @@ class Solution {
     }
 }
 2. Find the Nth root of a number using binary search
+    class Solution {
+    public int nthRoot(int n, int m) {
+        // code here
+        int l = 1;
+        int h = m;
+        while(l <= h){
+            int mid = l + (h-l)/2;
+            if((int)Math.pow(mid,n) == m){
+                return mid;
+            }
+            else if((int)Math.pow(mid,n) < m){
+                l = mid + 1;
+            }
+            else{
+                h = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
 
